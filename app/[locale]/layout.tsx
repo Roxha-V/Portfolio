6134cc27398569353c0ade721b848c46087691cb
Lucy BB <rociovalverde1@gmail.com>
2025-08-13 12:51:54 -0300

    Elimina la carga de mensajes en el componente de diseño de idioma, optimizando así el rendimiento al evitar una llamada innecesaria a la función getMessages().

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -25,7 +25,6 @@ export default async function LocaleLayout({
   children: React.ReactNode
   params: { locale: string }
 }) {
-  const messages = await getMessages()
 
   return (
     <html lang={locale}>
